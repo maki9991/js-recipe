e9dcbb6dd9da24c2a68d1c3b86e94ffdab425d47
maki9991 <tdm_yamada@cc.nara-wu.ac.jp>
2021-07-04 13:28:15 +0900

    [ストップウォッチ]を学習しました　よくわからん…

---
+++ 02_レシピ/01_ストップウォッチ/main.js
@@ -0,0 +1,26 @@
+const display = document.getElementById("display")
+const button = document.getElementById("button")
+
+let count = 0
+const countUp = function() {
+  //countを更新
+  count += 1
+  //countを秒単位にして表示
+  display.textContent = count / 100
+}
+
+let id = null
+
+button.onclick = function() {
+  if (id === null) {
+    //****************************START
+    //10msごとにcountUpを実行するように登録する
+    id = setInterval(countUp, 10)
+    //setInterval(関数,間隔)//間隔の単位は1/1000秒(=1ms)
+    button.textContent = "stop"
+  } else {
+    //****************************STOP
+    clearInterval(id)
+    id = nullbutton.textContent = "start"
+  }
+}
