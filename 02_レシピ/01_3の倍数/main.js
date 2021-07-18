36b4f0668e127d8c787bd238c7c5140bfef78d49
maki9991 <tdm_yamada@cc.nara-wu.ac.jp>
2021-07-18 14:11:48 +0900

    「3が大好き」を学習しました

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,10 +1,11 @@
 const genkiFunction = function(number) {
   //nを1からnumberまで1ずつ増やしながらfor文の中身を繰り返す
   for (let n = 1; n <= number; n++) {
-    if (n % 3 == 0) {
+    if (/*n % 3 == 0 || */ /3/.test(n)) {
       console.log(n + "!!!!!!")
     } else {
       console.log(n)
     }
   }
 }
+genkiFunction(34)
